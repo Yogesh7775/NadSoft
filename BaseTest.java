096cd1a1d9bb1b51ab737d9785ffd32884124fbb
Yogeshwar Shinde <121767046+Yogesh7775@users.noreply.github.com>
2025-04-10 17:38:50 +0530

    Create BaseTest.java

---
+++ BaseTest.java
@@ -0,0 +1,28 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeMethod
+    public void setUp(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
